5f9eae12b5b0602c9c5db1ac2907d7b4a944ad12
Thiago Rodrigues <thiagor_@live.com>
2021-04-04 16:26:30 -0300

    test: ensure DbListSpecifications throws if ListSpecificationsRepository throws

--- tests/application/use-cases/cars/db-list-specifications.spec.ts
+++ tests/application/use-cases/cars/db-list-specifications.spec.ts
@@ -1,5 +1,6 @@
 import { DbListSpecifications } from '@/application/use-cases';
 import { ListSpecificationsRepositorySpy } from '@/tests/application/mocks';
+import { throwError } from '@/tests/domain/mocks';
 
 const makeSut = () => {
   const listSpecificationsRepositorySpy = new ListSpecificationsRepositorySpy();
@@ -17,5 +18,15 @@ describe('DbListSpecifications', () => {
     await sut.list();
 
     expect(listSpecificationsRepositorySpy.count).toBe(1);
+  });
+
+  it('should throw if ListSpecificationsRepository throws', async () => {
+    const { sut, listSpecificationsRepositorySpy } = makeSut();
+    jest
+      .spyOn(listSpecificationsRepositorySpy, 'list')
+      .mockRejectedValueOnce(throwError);
+    const promise = sut.list();
+
+    await expect(promise).rejects.toThrow();
   });
 });
