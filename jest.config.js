f73129eacd3a53754982fb32635d44c3b9365d45
Thiago Rodrigues <thiagor_@live.com>
2021-04-14 21:18:34 -0300

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -85,7 +85,6 @@ module.exports = {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    '@/tests/(.*)': '<rootDir>/tests/$1',
     '@/(.*)': '<rootDir>/src/$1',
   },
 
